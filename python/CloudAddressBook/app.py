4a5f76f8b65d907a1f437e2723401ce328e11ff5
Pavel Berkovich <pberkovich1994@gmail.com>
2015-12-20 17:45:48 +0000

    Updated logging levels to INFO

--- python/CloudAddressBook/app.py
+++ python/CloudAddressBook/app.py
@@ -20,10 +20,10 @@ def checkin(userID, addr, port): # request to check in
         % (userID, addr + str(port), str(ts)))
     
     book_lock.acquire()
-    logging.debug("book_lock:acq")
+    logging.info("book_lock:acq")
     book[userID] = (addr + ":" + str(port), ts) # put into book
     book_lock.release()
-    logging.debug("book_lock:rel")
+    logging.info("book_lock:rel")
     
     return "ACK"
 
@@ -33,11 +33,11 @@ def lookup(userID): # lookup the address of a user
     logging.info("LOOKUP of user %s" % userID)
 
     book_lock.acquire()
-    logging.debug("book_lock:acq")
+    logging.info("book_lock:acq")
     if userID in book:
         rst = book[userID][0] # take first element of (addr, ts) tuple
     book_lock.release()
-    logging.debug("book_lock:rel")
+    logging.info("book_lock:rel")
     
     return rst
     
@@ -47,13 +47,13 @@ def display(): # for debugging purposes
     
     rst = ""
     book_lock.acquire()
-    logging.debug("book_lock:acq")
-    logging.debug("Address book contains %d entries" % len(book))
+    logging.info("book_lock:acq")
+    logging.info("Address book contains %d entries" % len(book))
     for userID in book:
         (addr, ts) = book[userID]
         rst += "%s %s <br>" % (userID.ljust(25), addr.ljust(25))
     book_lock.release()
-    logging.debug("book_lock:rel")
+    logging.info("book_lock:rel")
     
     return rst
     
@@ -66,24 +66,25 @@ def main():
 def validator():
     # runs in separate thread, constantly goes through the whole book
     # removing all items older than 7s
-    logging.debug("[VALIDATOR] thread started")
+    logging.info("[VALIDATOR] thread started")
     
     while(True):
-        logging.debug("[VALIDATOR] wakes up")
+        logging.info("[VALIDATOR] wakes up")
         book_lock.acquire()    
-        logging.debug("[VALIDATOR] book_lock:acq")
+        logging.info("[VALIDATOR] book_lock:acq")
         for userID in book.keys():
             (addr, ts) = book[userID]
             if(int(time.time() - ts) > 7):  # old
-                logging.debug("[VALIDATOR] deletes expired entry for %s"%userID)
+                logging.info("[VALIDATOR] deletes expired entry for %s"%userID)
                 del book[userID]            # remove
         book_lock.release()
-        logging.debug("[VALIDATOR] book_lock:rel")
+        logging.info("[VALIDATOR] book_lock:rel")
         time.sleep(5) # sleep for 8 sec
 
 
 if __name__ == "__main__":
     # start the validator
+    logging.info("Function main() started")
     th = threading.Thread(target = validator)
     th.setDaemon(True) # kill thread when prog exits
     th.start()
